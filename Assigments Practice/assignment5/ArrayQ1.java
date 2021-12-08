7687c11fd83b504e2e72d29839959295b1f25a82
Suyeshpatil <62711201+Suyeshpatil@users.noreply.github.com>
2021-12-08 13:04:17 +0530

    Add files via upload

---
+++ Assigments Practice/assignment5/ArrayQ1.java
@@ -0,0 +1,12 @@
+package assignment5;
+
+public class ArrayQ1 {
+public static void main(String[] args) {
+	int[] arr;
+	arr= new int [10];
+	for(int i=0;i<arr.length;i++) {
+		arr[i]=i;
+		System.out.print(arr[i] + " ");	
+	}
+}
+}
---
+++ Assigments Practice/assignment5/StrQ2.java
@@ -0,0 +1,9 @@
+package assignment5;
+
+public class StrQ2 {
+			public static void main(String[] args) {
+				String str[] = {"geeks","for","geeks"};
+				System.out.print(str[0] +str[1]+str[2]);
+				
+			}
+}
