0c1e2bc7a9554f58c172fb2e32a272780cd9f2f8
Suyeshpatil <62711201+Suyeshpatil@users.noreply.github.com>
2021-12-15 16:25:12 +0530

    Add files via upload

---
+++ Assigments Practice/assignment9/vowel.java
@@ -0,0 +1,16 @@
+package assignment9;
+
+public class vowel {
+	
+public static void main(String[] args) {
+		char ch ='a';
+		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) {
+            		System.out.println("Character "+ch+" is a Vowel");
+					}
+		else {
+			System.out.println("Character "+ch+" is a Consonant");
+			}
+	}
+}
+//output
+//Character a is a Vowel
