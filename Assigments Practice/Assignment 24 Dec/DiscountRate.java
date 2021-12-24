396f3a2adc7038efe0fd018fe779f0eab8d1b127
Suyeshpatil <62711201+Suyeshpatil@users.noreply.github.com>
2021-12-25 03:49:47 +0530

    Add files via upload

---
+++ Assigments Practice/Assignment 24 Dec/DiscountRate.java
@@ -0,0 +1,46 @@
+package beautyproduct;
+
+import java.util.Scanner;
+
+public class DiscountRate {
+	static float findFinalPricePremieme(float price) {
+		int discount = 20;
+		float discountprice = (price * discount) / 100;
+		return price - discountprice;
+	}
+
+	static float findFinalPriceGold(float price) {
+		int discount = 15;
+		float discountprice = (price * discount) / 100;
+		return price - discountprice;
+	}
+
+	static float findFinalPriceSilver(float price) {
+		int discount = 10;
+		float discountprice = (price * discount) / 100;
+		return price - discountprice;
+	}
+
+	public static void main(String[] args) {
+		int price;
+		int discount;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter price of the product :");
+		price = sc.nextInt();
+
+		System.out.println("Final price premieme after 20% Discount is" + findFinalPricePremieme(price));
+		System.out.println("Final price Gold after 15% Discount is " + findFinalPriceGold(price));
+		System.out.println("Final price Silver after 10% Discount is " + findFinalPriceSilver(price));
+	}
+
+} 
+
+
+//Output:-
+//Enter price of the product :
+//900
+//Final price premieme after 20% Discount is720.0
+//Final price Gold after 15% Discount is 765.0
+//Final price Silver after 10% Discount is 810.0
